b85b2b5afffe905855fb88b755de310d38f0c16a
LXJ9527 <154720034@qq.com>
2019-01-14 17:03:49 +0800

    no message

--- Sample06/Sample06/Program.cs
+++ Sample06/Sample06/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace Sample06
 {
@@ -6,7 +7,22 @@ namespace Sample06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Queue qObj = new Queue();
+
+            ////向队列中添加字符串数据
+            //qObj.Enqueue("Tom");
+            //qObj.Enqueue("Harry");
+            //qObj.Enqueue("Maria");
+            //qObj.Enqueue("john");
+
+            ////显示队列中的数据 
+            //while (qObj.Count != 0)
+            //{
+            //    Console.WriteLine(qObj.Dequeue());
+            //}
+            Console.WriteLine("800/5="+800 / 5);
+
+            Console.ReadKey();
         }
     }
 }
